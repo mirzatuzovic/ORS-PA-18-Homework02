7cbc2d5f858077e389678a15cc5ea4f015144c65
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-02-26 00:15:06 +0100

    Task 1

--- task1.py
+++ task1.py
@@ -15,11 +15,17 @@
 ===================================================
 """
 
+#da li string moze da se konvertuje u broj
+def  moze_li_u_float(string_broj):
+    if not isinstance(string_broj,str):
+        return False
 
-def main():
+    dozvoljeni_karakteri = ['0','1','2','3','4','5','6','7','8','9','.','-']
 
-    user_value = input("Enter string which will be evaluated: ")
-    print(can_string_be_float(user_value))
-    print(float(user_value))
+    for karakter in string_broj:
+        if karakter not in dozvoljeni_karakteri:
+           return False
 
-main()
+    return True
+
+print(moze_li_u_float("5"))
