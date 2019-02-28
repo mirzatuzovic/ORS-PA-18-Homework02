51fc1de3d877853bd4d7e7cc8118130b59e5438c
Mirza Tuzovic <Mirza.Tuzovic@udg.edu.me>
2019-02-28 23:20:57 +0100

    Task 2

--- task2.py
+++ task2.py
@@ -15,3 +15,47 @@
 * from previous task to validate user input.
 ===================================================
 """
+
+def can_string_be_float(user_value):
+
+    dozvoljeni_karakteri = ['0','1','2','3','4','5','6','7','8','9','.','-']
+
+    for ch in user_value:
+
+        if ch not in dozvoljeni_karakteri:
+            return False
+
+        broj_tacaka = 0
+
+        for ch in user_value:
+            if ch == '.':
+                broj_tacaka = broj_tacaka + 1
+
+        if broj_tacaka > 1:
+            return False
+
+        broj_minusa = 0
+
+        for ch in user_value:
+            if ch=='-':
+                broj_minusa = broj_minusa + 1
+
+        if broj_minusa > 1:
+            return False
+
+        if broj_minusa == 1:
+            if user_value[0] != '-':
+                return False
+
+    return True
+
+def main():
+
+    user_value= (input("Unesite duzinu u kilometrima: "))
+
+    if can_string_be_float(user_value):
+        print("Duzina u miljama je: ", float(user_value) * 0.6214)
+    else:
+        print("Unijeli ste pogresan podatak!")
+
+main()
